cc42e96cc225d7ca609cd0ce7af59d86ec2301b6
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2024-08-29 08:38:18 +0900

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ProgressLens-Auth-ProtoType",
+  title: "ProgressLens",
   description: "",
 };
 
