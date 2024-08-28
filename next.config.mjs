a0f4aa0f7e6d4335b73b5d2a3140da6a08bf38c6
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2024-08-28 17:58:35 +0900

    Set up project dependencies.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "placehold.jp" },
+      { protocol: "https", hostname: "images.microcms-assets.io" },
+      { protocol: "http", hostname: "localhost" },
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
