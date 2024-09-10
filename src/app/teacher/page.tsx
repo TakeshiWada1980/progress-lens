67cde775b6c78d0f26e15fb4c2fc76c30e2e6fce
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2024-09-10 22:27:45 +0900

    update page.tsx

--- src/app/teacher/page.tsx
+++ src/app/teacher/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Link from "@/app/_components/elements/Link";
 import PageTitle from "@/app/_components/elements/PageTitle";
 
 const Page: React.FC = () => {
@@ -7,6 +8,12 @@ const Page: React.FC = () => {
     <div>
       <PageTitle title="教員のトップページ" />
       <p>「教員」のためのトップページです。</p>
+
+      <ul className="mt-4">
+        <li>
+          <Link href="/teacher/sessions" label="/teacher/sessions" />
+        </li>
+      </ul>
     </div>
   );
 };
